27795473772b4d3af0ede59937685c1c1278d6ff
Nic30 <nic30@seznam.cz>
2018-04-11 00:41:06 +0200

    initial implementation of vcd parser and to json conversion

---
+++ pyDigitalWaveTools/vcd/parser.py
@@ -0,0 +1,243 @@
+#!python
+'''
+  :note: inspired by https://github.com/GordonMcGregor/vcd_parser/blob/master/vcd/parser.py
+
+  A basic self contained VCD parser object
+
+  Walks through the definitions constructing the appropriate signal references.
+  Caches XMR paths if and when the signal value changes in the dump for future reference.
+  Needs some hooks for callbacks on signal changes and methods to allow sampling of a signal with an appropriate clock reference
+
+  Refer to IEEE SystemVerilog standard 1800-2009 for VCD details Section 21.7 Value Change Dump (VCD) files
+'''
+
+from collections import defaultdict
+from itertools import dropwhile
+
+
+class VcdSyntaxError(Exception):
+    pass
+
+
+class VcdSignalScope():
+    def __init__(self, name, parent=None):
+        self.name = name
+        self.parent = parent
+        self.children = {}
+
+    def _getDebugName(self):
+        buff = []
+        o = self
+        while True:
+            buff.append(o.name)
+            o = o.parent
+            if o is None:
+                break
+        return ".".join(reversed(buff))
+
+    def toJson(self):
+        return {
+            "name": self.name,
+            "children": {n: ch.toJson() for n, ch in self.children.items()}
+        }
+
+    def __repr__(self):
+        return "<%s %s>" % (self.__class__.__name__, self._getDebugName())
+
+
+class VcdSignalInfo():
+    def __init__(self, vcdId, name, width, sigType, parent):
+        self.vcdId = vcdId
+        self.name = name
+        self.width = width
+        self.sigType = sigType
+        self.parent = parent
+        self.data = []
+
+    def toJson(self):
+        return {"name": self.name,
+                "type": {"width": self.width,
+                         "sigType": self.sigType},
+                "data": self.data}
+
+    def __repr__(self):
+        return "<%s %s vcdId:%s>" % (
+            self.__class__.__name__, VcdSignalScope._getDebugName(self), self.vcdId)
+
+
+class VcdParser(object):
+    '''
+    A parser object for VCD files.
+    Reads definitions and walks through the value changes
+
+    :ivar keyword_dispatch: dictionary {keyword: parse function}
+    :ivar scope: actual VcdSignalInfo
+    :ivar now: actula time (int)
+    :ivar idcode2series: dictionary {idcode: series} where series are list of tuples (time, value)
+    :ivar signals: dict {topName: VcdSignalInfo instance}
+    '''
+
+    def __init__(self):
+
+        keyword_functions = {
+            # declaration_keyword ::=
+            "$comment":        self.drop_declaration,
+            "$date":           self.save_declaration,
+            "$enddefinitions": self.vcd_enddefinitions,
+            "$scope":          self.vcd_scope,
+            "$timescale":      self.save_declaration,
+            "$upscope":        self.vcd_upscope,
+            "$var":            self.vcd_var,
+            "$version":        self.save_declaration,
+            # simulation_keyword ::=
+            "$dumpall":        self.vcd_dumpall,
+            "$dumpoff":        self.vcd_dumpoff,
+            "$dumpon":         self.vcd_dumpon,
+            "$dumpvars":       self.vcd_dumpvars,
+            "$end":            self.vcd_end,
+        }
+
+        self.keyword_dispatch = defaultdict(
+            self.parse_error, keyword_functions)
+
+        self.scope = None
+        self.now = 0
+        self.idcode2series = {}
+        self.end_of_definitions = False
+
+    def value_change(self, vcdId, value):
+        '''append change from VCD file signal data series'''
+        self.idcode2series[vcdId].append((self.now, value))
+
+    def parse(self, file_handle):
+        '''
+        Tokenize and parse the VCD file
+
+        :ivar file_handle: opened file with vcd string
+        '''
+        # open the VCD file and create a token generator
+        lineIterator = iter(enumerate(file_handle))
+        tokeniser = ((lineNo, word) for lineNo, line in lineIterator
+                     for word in line.split() if word)
+
+        while True:
+            token = next(tokeniser)
+            # parse VCD until the end of definitions
+            self.keyword_dispatch[token[1]](tokeniser, token[1])
+            if self.end_of_definitions:
+                break
+
+        while True:
+            try:
+                lineNo, token = next(lineIterator)
+            except StopIteration:
+                break
+
+            # parse changes
+            c = token[0]
+            if c == '$':
+                # skip $dump* tokens and $end tokens in sim section
+                continue
+            elif c == '#':
+                # [TODO] may be a float
+                self.now = int(token[1:])
+            else:
+                sp = token.split()
+                sp_len = len(sp)
+                if sp_len == 1:
+                    # 1 bit value
+                    value = c
+                    vcdId = token[1:]
+                elif sp_len == 2:
+                    # vectors and strings
+                    value, vcdId = sp
+                else:
+                    raise VcdSyntaxError(
+                        "Line %d: Don't understand: %s " % (lineNo, token))
+
+                self.value_change(vcdId.strip(), value.strip())
+
+    def parse_error(self, tokeniser, keyword):
+        raise VcdSyntaxError("Don't understand keyword: ", keyword)
+
+    def drop_declaration(self, tokeniser, keyword):
+        next(dropwhile(lambda x: x[1] != "$end", tokeniser))
+
+    def read_while_end(self, tokeniser):
+        for _, word in tokeniser:
+            if word == "$end":
+                return
+            else:
+                yield word
+
+    def save_declaration(self, tokeniser, keyword):
+        self.__setattr__(keyword.lstrip('$'),
+                         " ".join(self.read_while_end(tokeniser)))
+
+    def vcd_enddefinitions(self, tokeniser, keyword):
+        self.end_of_definitions = True
+        self.drop_declaration(tokeniser, keyword)
+
+    def vcd_scope(self, tokeniser, keyword):
+        scopeType = next(tokeniser)
+        assert scopeType[1] == "module", scopeType
+        scopeName = next(tokeniser)
+        assert next(tokeniser)[1] == "$end"
+        self.scope = VcdSignalScope(scopeName[1], self.scope)
+
+    def vcd_upscope(self, tokeniser, keyword):
+        self.scope = self.scope.parent
+        assert next(tokeniser)[1] == "$end"
+
+    def vcd_var(self, tokeniser, keyword):
+        data = tuple(self.read_while_end(tokeniser))
+        # ignore range on identifier ( TODO  Fix this )
+        (var_type, size, vcdId, reference) = data[:4]
+        parent = self.scope
+        info = VcdSignalInfo(vcdId, reference, size, var_type, parent)
+        assert vcdId not in self.idcode2series
+        assert reference not in parent.children
+        parent.children[reference] = info
+        self.idcode2series[vcdId] = info.data
+
+    def vcd_dumpall(self, tokeniser, keyword):
+        pass
+
+    def vcd_dumpoff(self, tokeniser, keyword):
+        pass
+
+    def vcd_dumpon(self, tokeniser, keyword):
+        pass
+
+    def vcd_dumpvars(self, tokeniser, keyword):
+        pass
+
+    def vcd_end(self, tokeniser, keyword):
+        if not self.end_of_definitions:
+            raise VcdSyntaxError("missing end of declaration section")
+
+
+if __name__ == '__main__':
+    import json
+    import sys
+    argc = len(sys.argv)
+    if argc == 1:
+        fIn = "../tests/AxiRegTC_test_write.vcd"
+    elif argc == 2:
+        fIn = sys.argv[1]
+    elif argc == 3:
+        fIn = sys.argv[1]
+        fOut = sys.argv[2]
+    else:
+        raise ValueError(sys.argv)
+
+    with open(fIn) as vcd_file:
+        vcd = VcdParser()
+        vcd.parse(vcd_file)
+        data = vcd.scope.toJoson()
+
+        if argc == 3:
+            with open(fOut, 'w') as jsonFile:
+                jsonFile.write(data)
+        else:
+            print(json.dumps(data))
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    #user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        import shlex
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+setup(name='pyDigitalWaveTools',
+      version='0.1',
+      description='python library for operations with VCD and other digital wave files ',
+      url='https://github.com/Nic30/pyDigitalWaveTools',
+      author='Michal Orsak',
+      author_email='michal.o.socials@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      package_data={'pyDigitalWaveTools': ['*.vcd', ]},
+      include_package_data=True,
+      zip_safe=False,
+      tests_require=['pytest'],
+      # test_suite='pyDigitalWaveTools.tests.all.suite',
+      )
