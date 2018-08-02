f53c691c4ba10c2448eb848d5432cd5ef8ec1c30
Nic30 <nic30@seznam.cz>
2018-08-02 09:12:29 +0200

    add classifiers

--- setup.py
+++ setup.py
@@ -3,35 +3,32 @@
 
 from os import path
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-import sys
-
+import textwrap
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-
-
-class PyTest(TestCommand):
-    # user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def run_tests(self):
-        import shlex
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
-
 
 setup(name='pyDigitalWaveTools',
       version='0.3',
       description='python library for operations with VCD and other digital wave files ',
       long_description=long_description,
       long_description_content_type="text/markdown",
+      classifiers=textwrap.dedent("""
+        Development Status :: 4 - Beta
+        Intended Audience :: Developers
+        License :: OSI Approved :: MIT License
+        Operating System :: OS Independent
+        Programming Language :: Python :: 3 :: Only
+        Programming Language :: Python :: 3
+        Programming Language :: Python :: 3.5
+        Programming Language :: Python :: 3.6
+        Programming Language :: Python :: 3.7
+        Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)
+        Topic :: System :: Hardware
+        Topic :: System :: Emulators
+        Topic :: Utilities
+        """).strip().splitlines(),
       url='https://github.com/Nic30/pyDigitalWaveTools',
       author='Michal Orsak',
       author_email='michal.o.socials@gmail.com',
@@ -41,5 +38,4 @@ setup(name='pyDigitalWaveTools',
       include_package_data=True,
       zip_safe=False,
       tests_require=['pytest'],
-      # test_suite='pyDigitalWaveTools.tests.all.suite',
-      )
+)
