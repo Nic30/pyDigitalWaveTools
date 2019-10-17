160d7478bee74f65608fda64bac691070b4acc9b
nic30 <nic30@seznam.cz>
2019-10-17 14:57:19 +0200

    setup.py update classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 
 from os import path
 from setuptools import setup, find_packages
-import textwrap
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
@@ -11,24 +10,20 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(name='pyDigitalWaveTools',
       version='0.3',
-      description='python library for operations with VCD and other digital wave files ',
+      description='python library for operations with VCD and other digital wave files',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      classifiers=textwrap.dedent("""
-        Development Status :: 4 - Beta
-        Intended Audience :: Developers
-        License :: OSI Approved :: MIT License
-        Operating System :: OS Independent
-        Programming Language :: Python :: 3 :: Only
-        Programming Language :: Python :: 3
-        Programming Language :: Python :: 3.5
-        Programming Language :: Python :: 3.6
-        Programming Language :: Python :: 3.7
-        Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)
-        Topic :: System :: Hardware
-        Topic :: System :: Emulators
-        Topic :: Utilities
-        """).strip().splitlines(),
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+        "Topic :: System :: Hardware",
+        "Topic :: System :: Emulators",
+        "Topic :: Utilities",
+      ],
       url='https://github.com/Nic30/pyDigitalWaveTools',
       author='Michal Orsak',
       author_email='michal.o.socials@gmail.com',
@@ -37,5 +32,4 @@ setup(name='pyDigitalWaveTools',
       package_data={'pyDigitalWaveTools': ['*.vcd', ]},
       include_package_data=True,
       zip_safe=False,
-      tests_require=['pytest'],
-)
+      tests_require=['pytest'])
